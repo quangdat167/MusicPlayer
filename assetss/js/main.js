855f582953f8ac4b638b4d041470ff46671a488a
unknown <datvg167@gmail.com>
2022-12-14 17:48:43 +0700

    finish

--- assetss/js/main.js
+++ assetss/js/main.js
@@ -8,21 +8,23 @@
  * 7. Next / Repeat when ended -> OK
  * 8. Active song -> OK
  * 9. Scroll active song into view
- * 10. Play song when click
+ * 10. Play song when click -> OK
  */
 
 
 	const $ = document.querySelector.bind(document)
 	const $$ = document.querySelectorAll.bind(document)
 
+	const PLAYER_STORAGE_KEY = 'Dat'
+
 	const allSong = $$('.song')
 	
 	const totalAudio = $('.time-audio')
-	const timeHover = $('.time-hover')
 	const cd = $('.cd')
 	const heading = $('header h2')
 	const cdThumb = $('.cd .cd-thumb')
 	const audio = $('#audio')
+	const video = $('#video')
 	const playBtn = $('.btn-toggle-play')
 	const player = $('.player')
 	const progress = $('#progress')
@@ -31,171 +33,229 @@
 	const randomBtn = $('.btn-random')
 	const repeatBtn = $('.btn-repeat')
 	const dashboard = $('.dashboard')
-
+	const playlist = $('.playlist')
+	
 	const app = {
-		currenIndex: 0,
+		currentIndex: 0,
 		isPlaying: false,
 		isRandom: false,
 		isRepeat: false,
+
+		config: JSON.parse(localStorage.getItem(PLAYER_STORAGE_KEY)) || {},
+
+		setConfig: function(key, value) {
+			this.config[key] = value
+			localStorage.setItem(PLAYER_STORAGE_KEY, JSON.stringify(this.config))
+		},
 
 		songs: [
 			{
 				name: 'Waiting for du',
 				singer: 'MOLO',
 				path: 'https://audio.jukehost.co.uk/T9NTNQJh7kCFVX77ZO8BoITWjli00k4Q',
-				image: './assetss/image/1.jpg'
+				image: './assetss/image/1.jpg',
+				backgroundImage: './assetss/image/1.jpg'
 			},
 			{
 				name: 'In the name of love',
 				singer: 'MARTIN GARIX X BEBE REXHA',
 				path: 'https://audio.jukehost.co.uk/Ww2CXsanXtQsUZfNvE8pxbuHIvQwwgjq',
-				image: './assetss/image/2.jpg'
+				image: './assetss/image/2.jpg',
+				backgroundImage: './assetss/image/2.jpg'
+
 			},
 			{
 				name: 'Santa tell me',
 				singer: 'TATUM',
 				path: 'https://audio.jukehost.co.uk/JkiUDSPcEw6onVRI6rozH9mKkBB0HKJU',
-				image: './assetss/image/3.jpg'
+				image: './assetss/image/3.jpg',
+				backgroundImage: './assetss/image/3.jpg'
+
 			},
 			{
 				name: 'Độ Tộc 2',
 				singer: 'ĐỘ MIXI x Phúc Du x Pháo x Masew',
 				path: 'https://audio.jukehost.co.uk/gWgIRaivqAUqsztPAhFu9xFZIpLWN0zX',
-				image: './assetss/image/4.jpg'
+				image: './assetss/image/4.jpg',
+				backgroundImage: './assetss/image/4.jpg'
+
 			},
 			{
 				name: 'Stream đến bao giờ',
 				singer: 'ĐỘ MIXI',
 				path: 'https://audio.jukehost.co.uk/FOZUkSfMH1LyfTdcE1kmiHPM2hAtfwdJ',
-				image: './assetss/image/5.jpg'
+				image: './assetss/image/5.jpg',
+				backgroundImage: './assetss/image/5.jpg'
+
 			},
-			{
-				name: 'Đạo lý tình yêu',
-				singer: 'Độ Lý',
-				path: 'https://audio.jukehost.co.uk/81zcK2ZhYSykveiAxDpsZdaw1qBTvRFl',
-				image: './assetss/image/6.jpg'
-			},
-			{
-				name: 'Cách tán CRUSH',
-				singer: 'MIXI tán gái',
-				path: 'https://audio.jukehost.co.uk/ern540dlzO4IjpL0WPYuZ70f8sv2BLNA',
-				image: './assetss/image/7.jpg'
-			},
+			// {
+			// 	name: 'Đạo lý tình yêu',
+			// 	singer: 'Độ Lý',
+			// 	path: 'https://audio.jukehost.co.uk/81zcK2ZhYSykveiAxDpsZdaw1qBTvRFl',
+			// 	image: './assetss/image/6.jpg',
+			// 	backgroundImage: './assetss/image/6.jpg'
+
+			// },
+			// {
+			// 	name: 'Cách tán CRUSH',
+			// 	singer: 'MIXI tán gái',
+			// 	path: 'https://audio.jukehost.co.uk/ern540dlzO4IjpL0WPYuZ70f8sv2BLNA',
+			// 	image: './assetss/image/7.jpg',
+			// 	backgroundImage: './assetss/image/7.jpg'
+
+			// },
 			{
 				name: 'Bên trên tầng lầu',
 				singer: 'Giấu tên',
 				path: 'https://audio.jukehost.co.uk/voPD8l4y2MtgXMEaTQLjuNGKfd6fScST',
-				image: './assetss/image/8.jpg'
+				image: './assetss/image/8.jpg',
+				backgroundImage: './assetss/image/8.jpg'
+
 			},
-			{
-				name: 'Phải có người yêu',
-				singer: 'Độ người yêu',
-				path: 'https://audio.jukehost.co.uk/w0T6Qm0Wa5Exbckw0NBYPzLRVdoC0XaW',
-				image: './assetss/image/9.jpg'
-			},
+			// {
+			// 	name: 'Phải có người yêu',
+			// 	singer: 'Độ người yêu',
+			// 	path: 'https://audio.jukehost.co.uk/w0T6Qm0Wa5Exbckw0NBYPzLRVdoC0XaW',
+			// 	image: './assetss/image/9.jpg',
+			// 	backgroundImage: './assetss/image/9.jpg'
+
+			// },
 			{
 				name: 'REMIX',
 				singer: 'DATTO SAN',
 				path: 'https://audio.jukehost.co.uk/UJQC1L2VtxB1VJUXrirPV2nd0esR3TTq',
-				image: './assetss/image/10.jpg'
-			},
-			{
-				name: 'Viet Deep #20',
-				singer: 'MEDIA_MusicPro',
-				path: 'https://audio.jukehost.co.uk/pZKDz3ojTleZ5P0oVbolQ6qZ9cTApVcZ',
-				image: './assetss/image/11.png'
+				image: './assetss/image/10.jpg',
+				backgroundImage: './assetss/image/10.jpg'
+
 			},
 			{
 				name: 'THE ONE THAT GOT AWAY REMIX ',
 				singer: '( TRUNG HOÀNG MIX ) HOT TIK TOK 2022',
 				path: 'https://audio.jukehost.co.uk/aqyXf57JMOuOiL1UYxdSVIyaVKwEiJhV',
-				image: './assetss/image/12.jpg'
+				image: './assetss/image/12.jpg',
+				backgroundImage: './assetss/image/12.jpg'
+
 			},
 			{
 				name: 'HOUSE LAK - DON\'T STOP - MIXSET',
 				singer: 'TeA DEEP',
 				path: 'https://audio.jukehost.co.uk/GpiaGGM4glp7dI5ik7o6TyREMAeqAgtw',
-				image: './assetss/image/13.png'
+				image: './assetss/image/13.png',
+				backgroundImage: './assetss/image/13.png'
+
 			},
 			{
 				name: 'HOUSE LAK 2022 - LAK WITH SOI DOLCE IN DA HEY Vol.2',
 				singer: 'TeA DEEP',
 				path: 'https://audio.jukehost.co.uk/9Gx7683oWxXsYKCB35CIbWvnti7EOcun',
-				image: './assetss/image/14.png'
+				image: './assetss/image/14.png',
+				backgroundImage: './assetss/image/14.png'
+
 			},
 			{
 				name: 'Deep House 2021 - Việt Mix Chuyện Tình Willzi | Deep Chill',
 				singer: 'TeA DEEP',
 				path: 'https://audio.jukehost.co.uk/1eGwSkq9cg8B2CuMunrjDwM4PSY394Xp',
-				image: './assetss/image/15.png'
+				image: './assetss/image/15.png',
+				backgroundImage: './assetss/image/15.png'
+
 			},
 			{
 				name: 'Mắt Em Nhòa Đi Mascara ( DJ Long Nhật ) - Đạt Con Mix',
 				singer: 'PHÚ THỌ BAY',
 				path: 'https://audio.jukehost.co.uk/GU34oKgF1ldD7L9xvRxC2Dh6v7m80ObZ',
-				image: './assetss/image/16.png'
+				image: './assetss/image/16.png',
+				backgroundImage: './assetss/image/16.png'
+
 			},
 			{
 				name: 'HOUSE LAK - THANH NIÊN | ANNIVERSARY - NBC',
 				singer: 'TeA DEEP',
 				path: 'https://audio.jukehost.co.uk/1IxR784CThbI21lbLigVCYg945TpMrOB',
-				image: './assetss/image/17.png'
+				image: './assetss/image/17.png',
+				backgroundImage: './assetss/image/17.png'
+
 			},
 			{
 				name: 'MIXTAPE - VIET DEEP CHỈ CÒN MỘT ĐÊM CUỐI',
 				singer: 'Trinh Minh Phuc',
 				path: 'https://audio.jukehost.co.uk/YsZ6qDhKWVUYXByklPOjFdhceQFzzhfN',
-				image: './assetss/image/18.png'
+				image: './assetss/image/18.png',
+				backgroundImage: './assetss/image/18.png'
+
 			},
 			{
 				name: 'MIXTAPE - VIET DEEP XA ANH CHẬM CHẬM THÔI',
 				singer: 'Trinh Minh Phuc',
 				path: 'https://audio.jukehost.co.uk/wzw3GdzFGe9QQRs0cNi7ZrU4Tx7xI0jZ',
-				image: './assetss/image/19.png'
+				image: './assetss/image/19.png',
+				backgroundImage: './assetss/image/19.png'
+
 			},
 			{
 				name: 'VIET DEEP 2022 - RẰNG EM MÃI Ở BÊN X NHƯ ANH ĐÃ THẤY EM',
 				singer: 'Deluxe Music',
 				path: 'https://audio.jukehost.co.uk/8Bpq95lmEuiJUBBNMTpmlIHmmzq3INSe',
-				image: './assetss/image/20.png'
+				image: './assetss/image/20.png',
+				backgroundImage: './assetss/image/20.png'
+
 			},
 			{
 				name: 'VIET DEEP 2022 - THỜI GIAN SẼ TRẢ LỜI X CHIẾC KHĂN GIÓ ẤM',
 				singer: 'Deluxe Music',
 				path: 'https://audio.jukehost.co.uk/Rs1wxf0XeVYACUirLHp4QJmzvpTIR2GU',
-				image: './assetss/image/21.png'
+				image: './assetss/image/21.png',
+				backgroundImage: './assetss/image/21.png'
+
 			},
 			{
 				name: 'HOUSE LAK 2022 - Thăng Trầm | Slow Down',
 				singer: 'TeA DEEP',
 				path: 'https://audio.jukehost.co.uk/AgdgaCvSRoNAFOA5t81sQYEGZwMs2xPB',
-				image: './assetss/image/22.png'
+				image: './assetss/image/22.png',
+				backgroundImage: './assetss/image/22.png'
+
 			},
 			{
 				name: 'MIXTAPE - HOUSE LAK | SO FAR AWAY - 1500 STROKES',
 				singer: 'Trinh Minh Phuc',
 				path: 'https://audio.jukehost.co.uk/FBKoedI9QpLrVeVzMUeBG7XMYsSbdsRw',
-				image: './assetss/image/23.png'
+				image: './assetss/image/23.png',
+				backgroundImage: './assetss/image/23.png'
+
 			},
 			{
 				name: 'VIET DEEP 2022 - RẰNG EM MÃI Ở BÊN REMIX',
 				singer: 'Deluxe Music',
 				path: 'https://audio.jukehost.co.uk/1HhhLRJvUFADFKcbsMIIby2HpLOsQm0d',
-				image: './assetss/image/24.png'
+				image: './assetss/image/24.png',
+				backgroundImage: './assetss/image/24.png'
+
+			},
+			{
+				name: 'Viet Deep #20',
+				singer: 'MEDIA_MusicPro',
+				path: 'https://audio.jukehost.co.uk/pZKDz3ojTleZ5P0oVbolQ6qZ9cTApVcZ',
+				image: './assetss/image/11.png',
+				backgroundImage: './assetss/image/11.png'
+
 			},
 			{
 				name: 'House Lak Nhà Làm - Thương em đến già By HoangMP Mix (ND Team)',
 				singer: 'MEDIA_MusicPro',
 				path: 'https://audio.jukehost.co.uk/vDzz9Y1mpRLBolq2twF3daWDfl782w90',
-				image: './assetss/image/25.png'
+				image: './assetss/image/25.png',
+				backgroundImage: './assetss/image/25.png'
+
 			},
 			
 		],
 
 		render: function() {
 			const htmls = this.songs.map((song, index) => {
+				// style="background: url(${song.backgroundImage}) center / cover no-repeat">
 				return `
-				<div class="song" id="song-${index}">
+				<div class="song" id="song-${index}" data-index="${index}">
 					<div class="thumb" 
 						style="background-image: url('${song.image}')">
 					</div>
@@ -209,19 +269,18 @@
 				</div>
 				`
 			})
-			$('.playlist').innerHTML = htmls.join('')
+			playlist.innerHTML = htmls.join('')
 
 		},
 		defineProperties: function() {
 			Object.defineProperty(this, 'currentSong', {
 				get: function() {
-					return this.songs[this.currenIndex]
+					return this.songs[this.currentIndex]
 				}
 			})
 		},
 
-		// Thanh thời gian Audio
-			
+		// Thanh thời gian Audio	
 		loadTimeAudio: 
 			function() {
 				setInterval(function() {
@@ -263,18 +322,13 @@
 					// }
 					// console.log(totalTime)
 					// totalAudio.textContent = `${currentTime} / ${totalTime}`	
-				}, 1000)
-
-				// Ẩn time-hover (Do Điện thoại dùng onmouseup không nhạy nên phải dùng cách này)
-				setInterval(function() {
-					timeHover.style.display = 'none'
-				}, 4000)
+				}, 1000)			
 				
 			},
 		
 		handleEvents: function() {
 			const _this = this
-			const cdWidth = cd.offsetWidth
+
 
 			// CD quay / stop
 			const cdThumbAnimate = cdThumb.animate([
@@ -289,6 +343,7 @@
 
 
 			// Scroll top
+			const cdWidth = cd.offsetWidth
 			document.onscroll= function() {
 				const scrollTop = window.scrollY || document.documentElement.scrollTop
 				const newCdWidth = cdWidth - scrollTop
@@ -300,11 +355,13 @@
 				_this.isPlaying = true
 				player.classList.add('playing')
 				cdThumbAnimate.play()
+
 			}
 			audio.onpause = function() {
 				_this.isPlaying = false
 				player.classList.remove('playing')
 				cdThumbAnimate.pause()
+
 			}
 			// Play / Pause 
 			playBtn.onclick = function() {
@@ -326,6 +383,8 @@
 					value = audio.currentTime / audio.duration *100
 				}
 				progress.value = value;
+				_this.setConfig('currentIndex', _this.currentIndex)
+				_this.setConfig('currentTime', audio.currentTime)
 			}
 				
 
@@ -346,39 +405,7 @@
 
 				if (currentHour != 0 ) totalTime = `${currentHour}:${currentMin}:${currentSec}`
 				else totalTime = `${currentMin}:${currentSec}`
-
-				timeHover.textContent = totalTime
-				timeHover.style.display = 'block'
-			}
-
-
-			// Khi thả chuột sẽ ẩn time-hover
-			progress.onmouseup = function(){
-				timeHover.style.display = 'none'
-			}
-
-			// let currentValue = progress.value
-			// progress.onmousemove = function() {
-			// 	if(progress.value !== currentValue) {
-			// 		let timeAudioHover = (Math.floor(progress.value / 100 * audio.duration))
-					
-			// 		const currentHour = Math.floor(timeAudioHover / 3600)
-			// 		let currentMin = Math.floor((timeAudioHover - currentHour*3600) / 60)
-			// 		let currentSec = Math.floor(timeAudioHover - currentHour*3600 - currentMin * 60)
-
-			// 		currentSec = currentSec < 10 ? ('0' + currentSec) : currentSec
-			// 		currentMin = currentMin < 10 ? ('0' + currentMin) : currentMin
-
-			// 		if (currentHour != 0 ) totalTime = `${currentHour}:${currentMin}:${currentSec}`
-					
-			// 		else totalTime = `${currentMin}:${currentSec}`
-
-			// 		timeHover.textContent = totalTime
-			// 		timeHover.style.display = 'block'
-			// 	}
-			// 	currentValue = progress.value
-			// }
-
+			}			
 
 			// NEXT song
 			nextBtn.onclick = () => {
@@ -389,6 +416,7 @@
 				}
 				audio.onplay()
 				audio.play()
+				_this.scrollIntoView()
 			}
 			//PREVIOUS SONG
 			prevBtn.onclick = () => {
@@ -399,17 +427,21 @@
 				}
 				audio.onplay()
 				audio.play()
+				_this.scrollIntoView()
 			}
 
 			// Xử lý Bật/Tắt RANDOM song
 			randomBtn.onclick = function() {
 				_this.isRandom = !_this.isRandom
 				randomBtn.classList.toggle('active')
+				_this.setConfig('isRandom', _this.isRandom)
 			}
 
+			// Xử lý Bật/Tắt REPEAT song
 			repeatBtn.onclick = function() {
 				_this.isRepeat = !_this.isRepeat
 				repeatBtn.classList.toggle('active')
+				_this.setConfig('isRepeat', _this.isRepeat)
 			}
 			
 			//Repet Song when ended
@@ -422,26 +454,25 @@
 			}
 
 			//Play Song when click
-			const allSong = $$('.song')
-			allSong.forEach(function(song, index) {
-				song.onclick = function(event) {
-					if(event.target.classList.value === 'option' || event.target.classList.value === 'fas fa-ellipsis-h' 
-						|| index === _this.currenIndex)
-						return;
-					_this.changeActiveSong(false)
-					_this.currenIndex = index
-					_this.loadCurrentSong()
-					audio.onplay()
-					audio.play()
+			playlist.onclick = function(e) {
+				const songNode = e.target.closest('.song:not(.active)')
+				if (songNode || e.target.closest('.option')) {
+					// Xử lý khi click vào option
+					if(e.target.closest('.option')) return 
+
+					// Xử lý khi click vào song
+					if(songNode) {
+						_this.changeActiveSong(false)
+						_this.currentIndex = Number(songNode.getAttribute('data-index'))
+						_this.loadCurrentSong()
+						audio.play()
+					}
+					
 				}
-			})
-			
-
-
-
-
+			}
 
 		},
+
 		loadCurrentSong: function() {
 			heading.textContent = this.currentSong.name
 			cdThumb.style.backgroundImage = `url('${this.currentSong.image}')`
@@ -452,9 +483,9 @@
 
 		},
 
+		// Active song 
 		changeActiveSong: function(active) {
-			const isSongStr = `#song-${this.currenIndex}`
-			const idSong = $(isSongStr)
+			const idSong = $(`#song-${this.currentIndex}`)
 			if(active)
 			idSong.classList.add('active')
 			else
@@ -462,18 +493,37 @@
 
 		},
 		
+		//Scroll Into View
+		scrollIntoView : function() {
+			setTimeout(()=> {
+				$('.song.active').scrollIntoView({
+					behavior: 'smooth',
+					block: 'center',
+				})
+			}, 200)
+		},
+
+		loadConfig: function() {
+			this.currentIndex = this.config.currentIndex
+			audio.currentTime = this.config.currentTime
+			this.isRandom = this.config.isRandom
+			this.isRepeat = this.config.isRepeat
+			this.scrollIntoView()
+		},
+
+		
 		nextSong: function() {
 			this.changeActiveSong(false)
-			this.currenIndex++
-			if(this.currenIndex >= this.songs.length) this.currenIndex = 0
+			this.currentIndex++
+			if(this.currentIndex >= this.songs.length) this.currentIndex = 0
 			
 			this.loadCurrentSong()
 		},
 		
 		prevSong: function() {
 			this.changeActiveSong(false)
-			this.currenIndex--
-			if(this.currenIndex == -1) this.currenIndex = this.songs.length - 1
+			this.currentIndex--
+			if(this.currentIndex == -1) this.currentIndex = this.songs.length - 1
 				this.loadCurrentSong()
 		},
 
@@ -482,9 +532,9 @@
 			let newCurrentIndex 
 			do {
 				newCurrentIndex = Math.floor(Math.random() * this.songs.length)
-			} while (this.currenIndex == newCurrentIndex)
+			} while (this.currentIndex == newCurrentIndex)
 
-			this.currenIndex = newCurrentIndex
+			this.currentIndex = newCurrentIndex
 			this.loadCurrentSong()
 		},
 
@@ -492,6 +542,8 @@
 		
 		
 		start: function() {
+			// Gán cấu hình từ config vào app
+			this.loadConfig()
 
 			//Định nghĩa các thuộc tính cho Object
 			this.defineProperties()
@@ -501,11 +553,13 @@
 			
 			// Lắng nghe/ Xử lý các event
 			this.handleEvents()
-
 			
 			// Tải bài hát đầu tiên vào UI
 			this.loadCurrentSong()
-
+			
+			// Khôi phục trạng thái của nút Random, Repeat
+			randomBtn.classList.toggle('active', this.isRandom)
+			repeatBtn.classList.toggle('active', this.isRepeat)
 		},
 
 	}
